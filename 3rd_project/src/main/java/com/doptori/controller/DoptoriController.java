1356d8fa95b8e1cf1d927fd8c30f0c10778fe3ee
yujeong9104 <yujung917@daum.net>
2023-01-05 14:24:10 +0900

    test

---
+++ 3rd_project/src/main/java/com/doptori/controller/DoptoriController.java
@@ -0,0 +1,21 @@
+package com.doptori.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DoptoriController {
+	
+	//@Autowired
+	//private DoptoriMapper mapper;
+	
+	@GetMapping("/Main.do")
+	public String Main() {
+		
+		return "basic";
+	}
+	
+	
+
+}
