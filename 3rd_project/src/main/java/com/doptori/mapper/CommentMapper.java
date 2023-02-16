d95c1954e76566178a49ad95696ca57655d6d563
user <user@DESKTOP-U59IHVT>
2023-02-16 10:34:24 +0900

    댓글 기능 구현(어려움)

--- 3rd_project/src/main/java/com/doptori/mapper/CommentMapper.java
+++ 3rd_project/src/main/java/com/doptori/mapper/CommentMapper.java
@@ -1,11 +1,31 @@
 package com.doptori.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.doptori.entity.Comment;
 
 @Mapper
 public interface CommentMapper {
+	
+	// 댓글 개수
+	public int commentCount();
+
+	// 댓글 목록
+	public List<Comment> commentList();
+
+	// 댓글 작성
+	public int commentInsert(Comment comment);
+
+	// 댓글 수정
+	public int commentUpdate(Comment comment);
+
+	// 댓글 삭제
+	public int commentDelete(int co_num);
+
+}
 
 	/*
 	 * public void commentInsert(Comment cvo);
@@ -16,4 +36,4 @@ public interface CommentMapper {
 	 * 
 	 */
 
-}
+
